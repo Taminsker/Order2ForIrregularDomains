6fbf6ae9f4841808f565755f8c970fe220364061
Élodie <elodyy.millan@gmail.com>
2020-03-17 20:18:08 +0100

    Ajout de FunToVec.cpp, petit modif de errors.cpp

--- src/O2FID/Tools/funtovec.cpp
+++ src/O2FID/Tools/funtovec.cpp
@@ -2,10 +2,34 @@
 
 Vector FunToVec (Mesh * mesh, double (*f) (Point, double), double t)
 {
+    /* Nombre de point totale dans le mesh à calculer. */
+    int n = mesh->GetNumberOfTotalPoints ();
+    /* Création d'un vecteur de n points. */
+    Vector Fvec (n);
 
+    for (int i = 0; i < n; i++)
+    {
+        /* On récupère le i-ème point du maillages. */
+        Point p_i = mesh->operator() (i);
+
+        /* On affecte la valeur de f à ce point. */
+        Fvec (i) = f(p_i, t);
+    }
+
+    return Fvec;
 }
 
 Vector FunToVec (Mesh * mesh, double value)
 {
+    /* Nombre de point totale dans le mesh à calculer. */
+    int n = mesh->GetNumberOfTotalPoints ();
+    /* Création d'un vecteur de n points. */
+    Vector Fvec (n);
 
+    for (int i = 0; i < n; i++)
+    {
+        Fvec (i) = value;
+    }
+
+    return Fvec;
 }
